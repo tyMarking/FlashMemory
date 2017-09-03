fffde6f6b45c49e0ce411fe24c1e7cec76b016c1
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2017-09-03 09:36:39 -0700

    Initial Commit

---
+++ Flash Memory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flash Memory
+//
+//  Created by Ty Marking on 9/3/17.
+//  Copyright © 2017 Ty Markijg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
